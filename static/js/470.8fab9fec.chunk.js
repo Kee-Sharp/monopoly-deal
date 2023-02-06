b41481dd2a89d6e2fbd594763f0e5b918b46ff1a
Kee-Sharp <Kee-Sharp@users.noreply.github.com>
2023-02-06 08:13:23 +0000

    deploy: dd1f74c18b9c8906ecea8aeacb49fe3696721816

---
+++ static/js/470.8fab9fec.chunk.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmonopoly_deal=self.webpackChunkmonopoly_deal||[]).push([[470],{9470:function(o,a,e){o.exports=e.p+"static/media/monopoly_man.a388665565ac743636a5.png"}}]);
